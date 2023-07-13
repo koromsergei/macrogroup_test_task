121c52b88de370fa455372362dd6ec76c562e947
Sergej Koromyslov <sergej.koromyslov@metalab.ifmo.ru>
2023-07-13 17:04:01 +0300

    Settings class announced

---
+++ Settings_task/settings.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class Settings
+{
+	public:
+		string input_pipeline;
+		vector<float> confidence;
+		vector<vector<vector<float>>> anchors;
+
+
+
+};
+
+
+void main()
+{
+	cout << "Hello"<<endl;
+
+}
