f6f9120f036ffd2ebddbc0d96d2b977240e0efd9
koromsergei <89305143+koromsergei@users.noreply.github.com>
2023-07-17 15:51:54 +0300

    vector 2

--- Settings_task/settings.cpp
+++ Settings_task/settings.cpp
@@ -23,100 +23,30 @@ class Settings
 		vector<float> confidence;
 		vector<vector<vector<float>>> anchors;
 
-		string temp_con;
-		string temp_anc;
+
 
 	int readIniFile()
 	{	
 		inipp::Ini<char> ini;
-		vector<vector<float>> anchors_unwrapped;
-
-
-
-		const char delim = '},{';
-
+	
 		ifstream is("test.ini");
 
 		ini.parse(is);
 
 		inipp::get_value(ini.sections[""], "input_pipeline", input_pipeline);
-		inipp::get_value(ini.sections[""], "confidence", temp_con);
-		inipp::get_value(ini.sections[""], "anchors", temp_anc);
-		
-
-		
-		// input_pipeline
+		inipp::get_value(ini.sections[""], "confidence", confidence);
+		inipp::get_value(ini.sections[""], "anchors", anchors);
+				
 		input_pipeline = input_pipeline.substr(1, input_pipeline.length() - 1);
 
-
-		// confidence
-		confidence = toVector(temp_con);
-
-		//anchors
-		stringstream ss(temp_anc);
-		string temp_anc_str;
-
-		while (getline(ss, temp_anc_str, delim))
-		{
-			cout << temp_anc_str << endl;
-			//anchors.push_back(toVectortwice(temp_anc_str));
-		}
 		return 0;
 	}
 
-
-	vector<float> toVector(string vec_str)
-	{	
-		vector<float> temp_vec;
-		vec_str = vec_str.substr(1, vec_str.length() - 1);
-
-		istringstream iss(vec_str);
-		string buf;
-
-		while (iss >> buf)
-			temp_vec.push_back(atof(buf.c_str()));
-		
-		/*
-				stringstream ss(vec_str);
-		string temp_anc_str;
-		const char delim = ', ';
-
-		while (getline(ss, temp_anc_str, delim))
-		{
-			temp_vec2.push_back(stof(temp_anc_str));
-		}
-
-		*/
-		
-		return temp_vec;
-
-	}
-
-
-	vector<vector<float>> toVectortwice(string vec_str)
-	{
-		vector<vector<float>> temp_vec;
-
-		temp_vec.push_back(toVector(vec_str));
-		
-		/*
-				for (const std::vector<float>& v : temp_vec)
-		{
-			for (float x : v) std::cout << x << ' ';
-			std::cout << std::endl;
-		}
-		
-		*/
-
-		return temp_vec;
 	
-	}
-
-
-	
-	void setIniFile()
+	int setIniFile(string upd_input_pipeline, vector<float> upd_confidence, vector<vector<vector<float>>> upd_anchors)
 	{
-		/*добавить проверку на существование файлов*/
+		//inipp::Ini<char> ini;
+
 		ifstream fin;
 		ofstream fout;
 
@@ -127,8 +57,6 @@ class Settings
 		{
 			fin.open("test.ini");
 			fout.open("test.ini.backup");
-
-
 			string i;
 			while (fin >> i)
 			{
@@ -137,15 +65,15 @@ class Settings
 			}
 		}
 
-		catch (const ifstream::failure & ex)
+		catch (const ifstream::failure& ex)
 		{
 			cout << ex.what() << endl;
 		}
-		
+
 		fin.close();
 		fout.close();
-		
 
+		return 0;
 	}
 
 
