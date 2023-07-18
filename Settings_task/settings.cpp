a53066a3f534b277ba71adb5be2593a9dff7446b
koromsergei <89305143+koromsergei@users.noreply.github.com>
2023-07-18 13:51:03 +0300

    for loop used

--- Settings_task/settings.cpp
+++ Settings_task/settings.cpp
@@ -4,13 +4,14 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-#include "ini.h"
-#include "INIReader.h"
+//#include "ini.h"
+//#include "INIReader.h"
 #include <fstream>
 #include "inipp.h"
 #include <typeinfo>
 #include <iterator>
 #include <cassert>
+#include <stack>
 #include <sstream>
 using namespace std;
 
@@ -31,32 +32,72 @@ class Settings
 		inipp::Ini<char> ini;
 		vector<vector<float>> anchors_unwrapped;
 
-		const char delim = ', ';
+		const char delim = '}},{';
 
 		ifstream is("test.ini");
 
 		ini.parse(is);
 
 		inipp::get_value(ini.sections[""], "input_pipeline", input_pipeline);
-		inipp::get_value(ini.sections[""], "confidence", confidence);
+		inipp::get_value(ini.sections[""], "confidence", temp_con);
 		inipp::get_value(ini.sections[""], "anchors", temp_anc);
 		
+		//input_pipeline
+		input_pipeline = input_pipeline.substr(1, input_pipeline.length() - 2);
+
 		// confidence
 		confidence = toVector(temp_con);
 
 		//anchors
-		stringstream ss(temp_anc);
+		temp_anc = temp_anc.substr(1, temp_anc.length() - 2);
+		cout << temp_anc << endl;
+
+		/*
+				stringstream ss(temp_anc);
 		string temp_anc_str;
 
 		while (getline(ss, temp_anc_str, delim))
 		{
-
+			cout << temp_anc_str ;
 			anchors.push_back(toVectortwice(temp_anc_str));
 		}
+		*/
 
-	
+
+		int first_index = 0;
+		int last_index = 0;
 
 
+		for (int i = 1; i < temp_anc.size(); ++i)
+		{
+			if (temp_anc[i] == '{')
+			{
+				
+				if (temp_anc[i - 1] == '{')
+				{
+					
+					first_index = i - 1;
+					//cout << first_index << endl;
+				}
+			}
+
+			if (temp_anc[i] == '}')
+			{
+				
+				if (temp_anc[i - 1] == '}')
+				{
+					
+					last_index = i + 1;
+					//cout << last_index << endl;
+					//anchors.push_back(toVectortwice(temp_anc.substr(first_index, last_index)));
+					cout << temp_anc.substr(first_index, last_index) << endl;
+				}
+			}
+
+
+		}
+
+		vector<vector<float>> a = toVectortwice("{{1, 2}, {3, 4}, {5, 6}}");
 
 		return 0;
 	}
@@ -65,17 +106,20 @@ class Settings
 	vector<float> toVector(string vec_str)
 	{	
 		vector<float> temp_vec;
+		cout << vec_str << endl;
+		vec_str = vec_str.substr(1, vec_str.length() - 2);
+		
+		const char delim = ',';
+		istringstream iss(vec_str);
+		string temp_str;
 
-		istringstream iss(temp_con);
-		string buf;
-
-		while (iss >> buf)
-			temp_vec.push_back(atof(buf.c_str()));
+		while (getline(iss, temp_str, delim))
+		{
+			temp_vec.push_back(stof(temp_str));
+		}
+		
 		return temp_vec;
 	}
-
-
-
 
 
 
@@ -83,15 +127,33 @@ class Settings
 	vector<vector<float>> toVectortwice(string vec_str)
 	{
 		vector<vector<float>> temp_vec;
-
-		temp_vec.push_back(toVector(vec_str));
+		vec_str = vec_str.substr(1, vec_str.length() - 2);
+		cout << vec_str << endl;
+		const char delim = '}, {';
 		
+		istringstream iss(vec_str);
+		string temp_str;
+		while (getline(iss, temp_str, delim))
+		{
+			cout << temp_str << endl;
+			temp_vec.push_back(toVector(temp_str));
+			
+		}
 
-		for (const std::vector<float>& v : temp_vec)
+		int first_index = 0;
+		int last_index = 0;
+
+
+
+		
+		/*
+				for (const std::vector<float>& v : temp_vec)
 		{
 			for (float x : v) std::cout << x << ' ';
 			std::cout << std::endl;
 		}
+		*/
+
 		return temp_vec;
 	}
 
@@ -141,8 +203,9 @@ int main()
 	a.readIniFile();
 
 	cout << a.input_pipeline << endl;
-	copy(a.confidence.begin(), a.confidence.end(), ostream_iterator<float>(cout, " "));
+	//copy(a.confidence.begin(), a.confidence.end(), ostream_iterator<float>(cout, ", "));
 	cout <<  endl;
+
 
 	return 0;
 }
